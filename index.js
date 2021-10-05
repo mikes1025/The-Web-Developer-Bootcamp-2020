fe92189403f14bb9935e641416f98f26f7db7603
mikes1025 <91908234+mikes1025@users.noreply.github.com>
2021-10-05 15:09:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Your code goes in here!
 let doneTodos = document.querySelectorAll('.done');
 let checkbox = document.querySelector("input[type='checkbox']");
+
+// Latest answer 10.5.2021
+ document.querySelector('img').src ='https://devsprouthosting.com/images/chicken.jpg';
+ document.querySelector('img').alt = 'chicken';
