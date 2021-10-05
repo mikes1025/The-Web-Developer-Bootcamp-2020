16475ceb9931d05790bf49b86ba2c2a4d161ea39
mikes1025 <91908234+mikes1025@users.noreply.github.com>
2021-10-05 15:32:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const image = document.querySelector('img');
 container.style.textAlign = 'center';
 image.style.width = '150px';
 image.style.borderRadius = '50%';
+
+//latest answer 10.5.2021
+document.querySelector("#container").style.textAlign = "center";
+document.querySelector("img").style.width = "150px";
+document.querySelector("img").style.borderRaduis = "50%";
